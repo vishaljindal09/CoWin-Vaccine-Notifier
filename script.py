a350b5361813d13672b33c08a0c7f1bd006eb67a
vishaljindal09 <vishaljindal09@gmail.com>
2021-05-15 15:34:58 +0530

    Update script.py
    Added telegram bot functionality. For telegram bot creation use Botfather and then create a telegram group and add that bot as an admin and get chat id. 
    Use Bot API and chat id in the code and run on local to get the message on telegram group

--- script.py
+++ script.py
@@ -1,17 +1,29 @@
-'''
-Script: Covid Vaccine Slot Availablity Notifier
-By Ayushi Rawat
-'''
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 15 14:40:46 2021
+
+@author: visha
+"""
 
 import requests
 
 from datetime import datetime, timedelta
-import time
+#
+def telegram_bot_send_text(bot_message):
+            bot_token = 'api'
+            bot_chat_id = 'group id starts with -ve sign'
+            send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chat_id + '&parse_mode=Markdown&text=' + bot_message
+            
+            print(send_text)
+        
+            response = requests.get(send_text)
+        
+            return response.json()
+        
 
-import json
 
-age = 52
-pinCodes = ["462003"]
+age = 55
+pinCodes = ["110085"]
 num_days = 2
 
 print_flag = 'Y'
@@ -38,6 +50,8 @@ while True:
 
             if result.ok:
                 response_json = result.json()
+                
+                print(response_json)
 
                 flag = False
                 if response_json["centers"]:            
@@ -50,16 +64,16 @@ while True:
 
                             for session in center["sessions"]:
                                 if (session["min_age_limit"] <= age and session["available_capacity"] > 0 ) :
-                                    print('Pincode: ' + pinCode)
-                                    print("Available on: {}".format(given_date))
-                                    print("\t", center["name"])
-                                    print("\t", center["block_name"])
-                                    print("\t Price: ", center["fee_type"])
-                                    print("\t Availablity : ", session["available_capacity"])
+                                    message = 'Pincode: ' + pinCode
+                                    message = message + "\nAvailable on:" + str(session["date"])
+                                    message = message + "\nSite: " + str(center["name"])
+                                    #print("\t", center["block_name"])
+                                    #print("\t Price: ", center["fee_type"])
+                                    message =message +"\nAvailablity : "+str(session["available_capacity"])
 
                                     if(session["vaccine"] != ''):
-                                        print("\t Vaccine type: ", session["vaccine"])
-                                    print("\n")
+                                        
+                                        response_telegram = telegram_bot_send_text(message)
 
                                     counter = counter + 1
                                 else:
@@ -75,9 +89,3 @@ while True:
         print("No Vaccination slot avaliable!")
     else:
         print("Search Completed!")
-
-
-    dt = datetime.now() + timedelta(minutes=3)
-
-    while datetime.now() < dt:
-        time.sleep(1)
